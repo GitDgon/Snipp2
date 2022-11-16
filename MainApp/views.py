e3fe0570143d0367649b967d69a9519d56caabd2
GitDgon <dgon_mail@mail.ru>
2022-11-16 20:17:09 +0300

    Форма добавления сниппета POST

--- MainApp/views.py
+++ MainApp/views.py
@@ -9,8 +9,20 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "POST":  #если пришел запрос на страничку pages/add_snippet.html POST
+        form_data = request.POST
+        snippet = Snippet(
+            name = form_data['name'],
+            lang = form_data['lang'],
+            code = form_data['code']
+        )
+        snippet.save() #сохраняем введенные данные
+        return redirect('snippet-list')
+        #print(f'{form_data=}')
+        #обработка данных формы
+    elif request.method == "GET":
+        context = {'pagename': 'Добавление нового сниппета!!!'}
+        return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
